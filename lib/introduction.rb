a1614494f9100c5e38e6f461abc50bb1c2fc5096
Olga  Chupp <olga.chupp@gmail.com>
2019-10-22 21:56:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,11 +11,8 @@ def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-def introduction_with_language_optional (name, language)
-   #puts language
-  if (!language)
+def introduction_with_language_optional (name)
    language="Ruby"
-  end
 puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
