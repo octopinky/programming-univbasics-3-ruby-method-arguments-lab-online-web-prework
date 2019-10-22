3acb07216c0824487599c19ba245034fe1f2e710
Olga  Chupp <olga.chupp@gmail.com>
2019-10-22 21:14:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-
+def introduction(name, language)
+  if (!language)
+    language="Ruby"
+  end
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
 
 
 
